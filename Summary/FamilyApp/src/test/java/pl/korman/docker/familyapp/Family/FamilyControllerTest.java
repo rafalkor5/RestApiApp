56851a84210af894248896b73aa3f8970b43088e
Rafał Korman <rafalkor5@gmail.com>
2022-06-11 21:59:06 +0200

    Controller Test

---
+++ Summary/FamilyApp/src/test/java/pl/korman/docker/familyapp/Family/FamilyControllerTest.java
@@ -0,0 +1,54 @@
+package pl.korman.docker.familyapp.Family;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FamilyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void familyGet_schouldReturnStatus200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/family/1"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    @Test
+    void familyPost_schouldReturnStatus201Created() throws Exception {
+        String content = "{\"familyName\":\"Piputuś\",\"nrOfAdults\":1,\"nrOfChildren\":1,\"nrOfInfants\":1," +
+                "\"familyMembersList\":[{\"givenName\":\"Mateusz\",\"familyName\":\"Piputuś\",\"age\":0}," +
+                "{\"givenName\":\"Andrzej\",\"familyName\":\"Piputuś\",\"age\":15}," +
+                "{\"givenName\":\"Izabela\",\"familyName\":\"Piputuś\",\"age\":16}]}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/family")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(201));
+    }
+
+    @Test
+    void familyPost_blankFamilyName_schouldReturnStatus400BadRequest() throws Exception {
+        String content = "{\"familyName\":\"\",\"nrOfAdults\":1,\"nrOfChildren\":1,\"nrOfInfants\":1," +
+                "\"familyMembersList\":[{\"givenName\":\"Mateusz\",\"familyName\":\"Piputuś\",\"age\":0}," +
+                "{\"givenName\":\"Andrzej\",\"familyName\":\"Piputuś\",\"age\":15}," +
+                "{\"givenName\":\"Izabela\",\"familyName\":\"Piputuś\",\"age\":16}]}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/family")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(400));
+    }
+
+
+}
--- Summary/FamilyMemberApp/src/test/java/pl/korman/docker/familymemberapp/FamilyMember/FamilyMemberControllerTest.java
+++ Summary/FamilyMemberApp/src/test/java/pl/korman/docker/familymemberapp/FamilyMember/FamilyMemberControllerTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -14,7 +18,45 @@ public class FamilyMemberControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void testExample(){
-
+    void familyMemberGet_schouldReturnStatus200oK() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/familymember/1"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(200));
     }
+
+    @Test
+    void familyMemberPost_schouldReturnStatus201Created() throws Exception {
+        String content = "{\"familyId\":2,\"givenName\":\"Andrzej\",\"familyName\":\"Kowalski\",\"age\":15}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/familymember")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                        .andDo(MockMvcResultHandlers.print())
+                        .andExpect(MockMvcResultMatchers.status().is(201));
+    }
+
+    @Test
+    void familyMemberPost_blankGivenName_schouldReturnStatus400BadRequest() throws Exception {
+        String content = "{\"familyId\":2,\"givenName\":\"\",\"familyName\":\"Kowalski\",\"age\":15}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/familymember")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                        .andDo(MockMvcResultHandlers.print())
+                        .andExpect(MockMvcResultMatchers.status().is(400));
+    }
+
+    @Test
+    void familyMemberPost_blankFamilyName_schouldReturnStatus400BadRequest() throws Exception {
+        String content = "{\"familyId\":2,\"givenName\":\"Andrzej\",\"familyName\":\"\",\"age\":15}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/familymember")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                        .andDo(MockMvcResultHandlers.print())
+                        .andExpect(MockMvcResultMatchers.status().is(400));
+    }
+
+
+
+
+
+
 }
