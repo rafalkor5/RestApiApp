5c7f42a57ae51ea122ff15e61e229589d5d6879e
Rafał Korman <rafalkor5@gmail.com>
2022-06-11 15:04:02 +0200

    Add validator BEAN and custom error from validator.

--- Summary/FamilyMemberApp/src/main/java/pl/korman/docker/familymemberapp/FamilyMemberAppApplication.java
+++ Summary/FamilyMemberApp/src/main/java/pl/korman/docker/familymemberapp/FamilyMemberAppApplication.java
@@ -2,12 +2,36 @@ package pl.korman.docker.familymemberapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
+import java.io.IOException;
 
 @SpringBootApplication
 public class FamilyMemberAppApplication {
+
+    @Bean
+    Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FamilyMemberAppApplication.class, args);
     }
 
+    @ControllerAdvice
+    public static class CustomErrorHandler {
+
+        @ExceptionHandler(ConstraintViolationException.class)
+        public void handleConstraintViolationException(ConstraintViolationException exception,
+                                                       ServletWebRequest webRequest) throws IOException {
+            webRequest.getResponse().sendError(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+        }
+    }
 }
